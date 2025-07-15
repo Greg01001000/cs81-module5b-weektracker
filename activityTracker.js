b6b55e4ff4832eae1810431b0498e946f9f3517a
Greg Hagen <gr-a@outlook.com>
2025-07-14 20:30:14 -0700

    Created activityTracker.js with myWeek object and prediction comments

---
+++ activityTracker.js
@@ -0,0 +1,19 @@
+// CS 81 Module 5 Assignment 5B: My Week in Data, GregH, 7/14/25
+
+let myWeek = [{day: 'Monday', activity: 'Schoolwork', category: 'learning', hoursSpent: 13, enjoyment: 7, timeOfDay: 'all day'},
+    {day: 'Tuesday', activity: 'Shave & shower', category: 'physical', hoursSpent: 3, enjoyment: 4, timeOfDay: 'afternoon'},
+    {day: 'Wednesday', activity: 'Bible study', category: 'learning', hoursSpent: 2, enjoyment: 8, timeOfDay: 'evening'},
+    {day: 'Thursday', activity: 'Bible meeting', category: 'social', hoursSpent: 3, enjoyment: 9, timeOfDay: 'evening'},
+    {day: 'Friday', activity: 'Shave & shower', category: 'physical', hoursSpent: 3, enjoyment: 4, timeOfDay: 'afternoon'},
+    {day: 'Saturday', activity: 'Ministry', category: 'social', hoursSpent: 2, enjoyment: 9, timeOfDay: 'morning'},
+    {day: 'Sunday', activity: 'Schoolwork', category: 'learning', hoursSpent: 13, enjoyment: 7, timeOfDay: 'all day'},]
+
+/* Which activity will have the highest enjoyment?
+* I predict that 'Bible meeting' and 'Ministry' will have the highest enjoyment.
+*
+* What category will dominate your week?
+* I predict that 'Schoolwork' will dominate my week.
+*
+* What patterns might exist around time of day?
+* I think I tend to do boring stuff in the afternoons; so I predict enjoyment will be higher in the mornings and evenings.
+*/
